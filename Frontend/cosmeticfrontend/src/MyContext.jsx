9ff95e5b1d2c2e5b6ddacfe7296a7a01318f06cc
SiddharamTalawar <siddharamtalawar10@gmail.com>
2024-07-21 19:48:06 +0530

    edited mycontext

--- Frontend/cosmeticfrontend/src/MyContext.jsx
+++ Frontend/cosmeticfrontend/src/MyContext.jsx
@@ -1,9 +1,29 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
+import api from './api';
  
 export const Context = createContext("");
 const  MyContext = ({children}) =>{
     const [cartItems, setcartItems] = useState([]);
-    return <Context.Provider value={{cartItems, setcartItems}}>{children}</Context.Provider>;
+    const [produstslist, setProdustslist] = useState([]); 
+    useEffect(() => {
+        getprodusts_list();
+    }, []);
+    // get products list from Backend
+    const getprodusts_list = () => {
+        // setcategory("");
+        // setsearch_data("");
+        api
+            .get("shop/products/")
+            .then((res) => res.data)
+            
+            .then((data) => {
+                
+                setProdustslist(data.results);
+                
+            })
+            .catch((err) => alert(err));
+    };
+    return <Context.Provider value={{cartItems, setcartItems, produstslist, setProdustslist}}>{children}</Context.Provider>;
         
     
 };
